6973abcd2175b1ba6c2dc1828d61f25c0d1161ef
Paulo Gabriel <138730369+paulogab2601@users.noreply.github.com>
2024-12-04 09:47:28 -0300

    New Update

---
+++ head/menus.h
@@ -0,0 +1,6 @@
+#ifndef MENUS_H
+#define MENUS_H
+
+void EditorCardapio(int *);
+
+#endif // MENU_H
--- main.c
+++ main.c
@@ -1,13 +1,38 @@
 // Incluindo bibliotecas propias:
 #include "head/ingredientes.h"
 #include "head/pizza.h"
+#include "head/menus.h"
 
 int main()
 {
-    int a = 0;
-    ingrediente i1 = newIngrediente(&a);
+    int select = 1; // Seletor do 1° menu
 
-    printf("%d", i1.id_ingrediente);
+    int pos_Pizza = 0, pos_Ingrediente = 0; // Posição dos vetores
+
+    printf("<------- Sistema Pizzaria Joao Fortao ------->");
+
+    while (select != 0)
+    {
+        printf("Qual operacao deseja realizar?");
+        printf("1- Editar Cardapio\n2- Editar Estoque\n3-Realizar venda\n0-Sair\n\n");
+        scanf("%d", &select);
+
+        switch (select)
+        {
+        case 1:
+            EditorCardapio(&pos_Pizza);
+            break;
+        case 0:
+            printf("Encerrando...");
+            exit(0);
+            break;
+
+        default:
+            printf("Selecione uma opcao valida!");
+            select = 1;
+            break;
+        }
+    }
 
     return 0;
 }
